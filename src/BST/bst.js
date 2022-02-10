3ab6aedafd0467000b9f6a20611958fdef490dfc
Poisk <vpisotski@gmail.com>
2022-02-10 14:16:59 -0800

    add method corrected

--- src/BST/bst.js
+++ src/BST/bst.js
@@ -1,4 +1,3 @@
-
 {
 
     "use strict"
@@ -16,49 +15,46 @@
          //determine where the node belongs to
         //according to BST properties
 
-        addNode (node, parent = this.root) {
+        swap (node1, node2) {
 
-            //iterate through the tree
-            //if nodes' value is greated than its parent
-            //store it on the left otherwise to the right
-            let nodeVal = node.value;
-            let parentVal = parent.value;
+            let archived = node1.value;
+            node1.value = node2.value;
+            node2.value = archived;
 
-            //BST should have no duplicate
-            if (parentVal === nodeVal) {
+            return {
+                
+                node1,
+                node2,
+            }
+        }
+        
+        //iterate through the tree
+        //if nodes' value is greated than its parent
+        //store it on the left otherwise to the right     
+        addNode (parent, node) {
+
+            //BST should have no duplicate values
+            if (parent.value === node.value) {
                 console.log(`duplicate value ${nodeVal} and ${parentVal}`);
                 return;
             }
-
-            //if parent already has a child on the place
-            //if nodes value is smaller swap with the left node and pass the left node to addNode
-            //if nodes value is bigger swap with the right node and pass the right node to addNode
             
-            let newParent;
-            let swap = function() {
-                node.value = parentVal;
-                parent.value = nodeVal;                
+            //if parent is greater than node
+            //node tries to add itself to the left
+                //if left is already taken
+                //node is trying to add itself to parents left
+        
+            if (parent.value > node.value) {
+                
+                if (!parent.left) { parent.left = node }                    
+                else { return this.addNode(parent.left, node) }
+                
+            } else {
+                
+                if (!parent.right) { parent.right = node }
+                else { return this.addNode(parent.right, node) }
             }
             
-            if (parentVal > nodeVal) {
-                
-                newParent = parent.left
-                if (!parent.left) { parent.left = node;
-                    return;
-                } else if ( parent !==this.root ) {
-                    swap();
-                }
-            } else {
-
-                newParent = parent.right
-                if (!parent.right) { parent.right = node;
-                    return;
-                } else if ( parent !==this.root ) {
-                    swap();
-                }
-             }
-            
-            this.addNode(node, newParent);
             return;
         }
 
@@ -76,11 +72,10 @@
 
             //iterate through the array
             this.root = this.createNode(array[0]);
-            console.log(`root created`)
             
             for (let i = 1; i < array.length; i = i + 1) {
                 let el = array[i];
-                this.addNode(this.createNode(el));
+                this.addNode(this.root, this.createNode(el));
             }
             //for each element create a node
             //add each node to the tree
@@ -99,12 +94,6 @@
      //the missing child also has to be replace
     }
 
-
-
-    
-
-
-
     //implement preOrder
     //implement postOrder
     //implement inOrder
@@ -116,9 +105,12 @@
     //be equal or (+-1 node) to the number of nodes on the left
 
 
-//     let birch = new BST([6,1,1,3,10,7,2,9,4,0]);
-    let birch = new BST([6,1,1,3,10,7]);
-
+    let birch = new BST([61,13,34,120,55,311,45,39,18,11,2,6,23,27,98,5,66]);
+    // let birch = new BST([6]);
+    // let n = [1,3,4,9,8]
+    // for (let i = 0; i < n.length; i = i + 1 ){
+    //     let node = birch.createNode(n[i])
+    //     birch.addNode(node)
+    // }
+    console.log(birch)
 }
-
-export default BST
