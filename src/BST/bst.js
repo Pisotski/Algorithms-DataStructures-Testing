925179e23bb09585adb6dc9991b1310ebd84c912
Poisk <vpisotski@gmail.com>
2022-02-10 17:00:52 -0800

    delete method added

--- src/BST/bst.js
+++ src/BST/bst.js
@@ -81,12 +81,9 @@
         search (node, value) {
             //search function takes value and returnes the corresponding node
             if(!node) {
-                console.log(`there is no node with value ${value}`);
                 return;
             }
             if (node.value === value) {
-                console.log(`here you go`);
-                console.log(node)
                 return node
             }
             
@@ -96,17 +93,37 @@
         deleteNode(value) {
             //takes value
             //searches for node
-            //when found deletes node
-// TODO: determine which branch to delete by serching the depths of two branches
+            let toBeDeleted = this.search(this.root, value);
 
+            if (!toBeDeleted) {
+                console.log(`node doesn't exist`)
+                return;
+            }
+
+            let deletedValue = toBeDeleted.value;
+
+
+            //when found deletes node
+// TODO: determine which branch to delete by searching the depths of two branches
             //detach left branch
             //replaces deleted node itself with the right branch
             //attach detached branch to the right branch
-            
-            //returns deleted node 
+            let detached;
+            if (toBeDeleted.right) {
+                
+                detached = toBeDeleted.left;
+                Object.assign(toBeDeleted, toBeDeleted.right);
+                this.addNode(toBeDeleted, detached)
+            } else {
+                
+                Object.assign(toBeDeleted, toBeDeleted.left);
+            }
+
+
+            console.log(`node ${value} erased`);
+            return 
         }
     }
-
     //implement preOrder
     //implement postOrder
     //implement inOrder
@@ -119,12 +136,7 @@
 
 
     let birch = new BST([61,13,34,120,55,311,45,39,18,11,2,6,23,27,98,5,66]);
-    // let birch = new BST([6]);
-    // let n = [1,3,4,9,8]
-    // for (let i = 0; i < n.length; i = i + 1 ){
-    //     let node = birch.createNode(n[i])
-    //     birch.addNode(node)
-    // }
+
     console.log(birch)
 
 }
