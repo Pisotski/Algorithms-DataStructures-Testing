de0d440053eb26637ec8aff06c93e3ff998e3bb5
Poisk <vpisotski@gmail.com>
2022-03-23 11:18:47 -0700

    Sliding Window practice p2

--- src/485. Max Consecutive Ones/findMaxConsecutiveOnes.js
+++ src/485. Max Consecutive Ones/findMaxConsecutiveOnes.js
@@ -28,4 +28,34 @@ var findMaxConsecutiveOnes = function (nums) {
     return globalMax;
 };
 
-export { findMaxConsecutiveOnes };
+// Given a binary array nums and an integer k, 
+//return the maximum number of consecutive 1's 
+// in the array if you can flip at most k 0's.
+
+// Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+// Output: 6
+// Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. 
+//     The longest subarray is underlined.
+
+// Sliding Window
+
+let longestOnes = function (nums, k) {
+
+    let left = 0, right = 0, len = nums.length, globalMax = 0, zeros = k;
+
+    while (right < len) {
+        if (nums[right] === 0) zeros -= 1;
+
+        while (zeros < 0) {
+            if (nums[left] === 0) zeros += 1;
+            left += 1;
+        }
+        right += 1;
+        globalMax = Math.max(globalMax, right - left);
+    }
+    return globalMax;
+};
+
+
+export { findMaxConsecutiveOnes, longestOnes };
--- src/485. Max Consecutive Ones/findMaxConsecutiveOnes.test.js
+++ src/485. Max Consecutive Ones/findMaxConsecutiveOnes.test.js
@@ -1,7 +1,13 @@
-import { findMaxConsecutiveOnes } from "./findMaxConsecutiveOnes";
+import { findMaxConsecutiveOnes, longestOnes } from "./findMaxConsecutiveOnes";
 
-test(`return the maximum number of consecutive 1's in the array`, () => {
+test(`should return the maximum number of consecutive 1's in the array`, () => {
     let nums = [1, 1, 0, 1, 1, 1];
     let ans = 3;
     expect(findMaxConsecutiveOnes(nums)).toEqual(ans);
+});
+
+test(`should return the maximum number of consecutive 1's in the array if you can flip at most k 0's`, () => {
+    let nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], k = 2;
+    let ans = 6;
+    expect(longestOnes(nums, k)).toEqual(ans);
 });
